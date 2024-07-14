e4b894fb915b7808703720bcb318f87a2de801f2
Tushar <tusharpersai@gmail.com>
2024-07-14 21:53:34 +0530

    ui design complete

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoHomeSharp } from "react-icons/io5";
+import { FaHeart } from "react-icons/fa";
+import { MdEditDocument, MdOutlineExplore } from "react-icons/md";
+import { PiSignInBold, PiSignOutBold } from "react-icons/pi";
+import Logout from "./LogOut";
+
+
+const Sidebar = () => {
+  const authUser =false;
+  return (
+    <aside className="flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto w-full bg-white-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-r hover:bg-gray-600/10 duration-200 text-white border-gray-500">
+      <nav className="h-full flex flex-col gap-5">
+        <Link to="/" className="flex justify-center">
+          <img src="/github.svg" alt="GitHub Logo" className="h-8" />
+        </Link>
+        <Link to="/" className="flex justify-center">
+          <IoHomeSharp size={20} />
+        </Link>
+        {authUser && (
+          <Link to="/likes" className="flex justify-center">
+            <FaHeart size={20} />
+          </Link>
+        )}
+        {authUser && (
+          <Link to="/explore" className="flex justify-center">
+            <MdOutlineExplore size={25} />
+          </Link>
+        )}
+
+        {!authUser && (
+          <Link to="/login" className="flex justify-center">
+            <PiSignInBold size={25} />
+          </Link>
+        )}
+
+        {!authUser && (
+          <Link to="/signup" className="flex justify-center">
+            <MdEditDocument size={25} />
+          </Link>
+        )}
+        {authUser && (
+          <div className="flex flex-col gap-2 mt-auto">
+            <Logout />
+          </div>
+        )} 
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;

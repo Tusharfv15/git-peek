e2642da8a6aacb103774018f106405d4b7a6d4ea
Tushar <tusharpersai@gmail.com>
2024-07-17 20:46:14 +0530

    updated

--- backend/passport/githubAuth.js
+++ backend/passport/githubAuth.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "/api/auth/github/callback",
+      callbackURL: "https://git-peek-app.onrender.com/api/auth/github/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       // asynchronous verification, for effect...
